78aa70718ef08b41d641a0720e64d454e95421c6
Manu George <manu@trackplex.com>
2020-11-17 00:02:00 +0530

    Added few components & installed few dependencies

---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Nav, NavbarContainer, NavLogo } from "./NavbarElement";
+
+const Navbar = () => {
+  return (
+    //Empty Tag reperesent react fragment
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">dolla</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
